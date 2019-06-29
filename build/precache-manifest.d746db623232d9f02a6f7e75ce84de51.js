55223fec75c94e1e74bfd6e9e0a0307ba345ed69
Faizy711 <faizan.s711@gmail.com>
2019-06-28 20:44:51 -0500

    github

---
+++ build/precache-manifest.d746db623232d9f02a6f7e75ce84de51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfd952a62223190c785b9ea82730519",
+    "url": "/threejs_cube/index.html"
+  },
+  {
+    "revision": "32f0de708e0df4b8b8fc",
+    "url": "/threejs_cube/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4beb21e5872c83cc42f9",
+    "url": "/threejs_cube/static/js/2.e544a2fc.chunk.js"
+  },
+  {
+    "revision": "32f0de708e0df4b8b8fc",
+    "url": "/threejs_cube/static/js/main.c144f028.chunk.js"
+  },
+  {
+    "revision": "b06b709a8ac2255f3c5b",
+    "url": "/threejs_cube/static/js/runtime~main.a48d7233.js"
+  }
+]);
